802743d76f23b27de207e49ca305d6d877e2a34f
AzevedoGabriel <gabriel.dantas.azevedo@ccc.ufcg.edu.br>
2022-08-16 14:59:30 -0300

    iniciando aplicação

---
+++ backend/agendacontatos/src/test/java/com/crud/demo/agendacontatos/AgendacontatosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.demo.agendacontatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendacontatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
