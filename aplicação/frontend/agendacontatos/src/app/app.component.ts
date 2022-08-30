8271f6f6de649c293db714c35814cbce04c01536
AzevedoGabriel <gabriel.dantas.azevedo@ccc.ufcg.edu.br>
2022-08-30 16:32:32 -0300

    Finished!

--- aplicação/frontend/agendacontatos/src/app/app.component.ts
+++ aplicação/frontend/agendacontatos/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Contato } from './contato';
 import { ContatoService } from './contato.service';
 
@@ -9,7 +10,9 @@ import { ContatoService } from './contato.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  public contatos!: Contato[];
+  public contatos: Contato[] = [];
+  public editContato: Contato | undefined;
+  public deleteContato: Contato | undefined;
 
   constructor(private contatoService: ContatoService) {}
 
@@ -28,6 +31,58 @@ export class AppComponent implements OnInit {
     );
   }
 
+  public onAddContato(addForm: NgForm): void {
+    document.getElementById('add-contato-form')?.click();
+    this.contatoService.addContato(addForm.value).subscribe(
+      (response: Contato) => {
+        console.log(response);
+        this.getContatos();
+        addForm.reset();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+        addForm.reset();
+      }
+    );
+  }
+
+  public onUpdateContato(contato: Contato): void {
+    this.contatoService.updateContato(contato).subscribe(
+      (response: Contato) => {
+        console.log(response);
+        this.getContatos();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onDeleteContato(contatoId: number): void {
+    this.contatoService.deleteContato(contatoId).subscribe(
+      (response: void) => {
+        console.log(response);
+        this.getContatos();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public searchContatos(key: string): void {
+    const results: Contato[] = [];
+    for (const contato of this.contatos) {
+      if (contato.name.toLowerCase().indexOf(key.toLowerCase()) !== -1){
+        results.push(contato);
+      }
+    }
+    this.contatos = results;
+    if (results.length === 0 || !key) {
+      this.getContatos();
+    }
+  }
+
   public onOpenModal(mode: string, contato?: Contato): void {
     const container = document.getElementById('main-container');
     const button = document.createElement('button');
@@ -38,9 +93,11 @@ export class AppComponent implements OnInit {
       button.setAttribute('data-target', '#addContatoModal');
     }
     if (mode === 'edit') {
+      this.editContato = contato;
       button.setAttribute('data-target', '#updateContatoModal');
     }
     if (mode === 'delete') {
+      this.deleteContato = contato;
       button.setAttribute('data-target', '#deleteContatoModal');
     }
     container?.appendChild(button);
